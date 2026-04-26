3f9f9291a937f768fd394c9dbd03d13c5ed6e37c
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-04-27 00:28:50 +0530

    payment controller

---
+++ Backend/controllers/payment.controller.js
@@ -0,0 +1,34 @@
+import paymentModel from "../models/payment.model";
+import razorpay from "../services/razorpay.services";
+
+export const createOrder = async (req, res) => {
+  try {
+    const { planId, amount, credits } = req.body;
+    if (!amount || !credits) {
+      return res.status(400).json({ message: "Invalid plan data" });
+    }
+
+    const options = {
+      amount: amount * 100,
+      currency: "INR",
+      receipt: `receipt_${Date.now()}`,
+    };
+
+    const order = await razorpay.orders.create(options);
+
+    await paymentModel.create({
+      userId: req.userId,
+      planId,
+      amount,
+      credits,
+      razorpayOrderId: order.id,
+      status: "created",
+    });
+
+    return res.json(order);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: `failed to create razorpay Order ${error}` });
+  }
+};
