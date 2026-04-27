a94cdbbec007e299b213203066a0ff70d22c6cf3
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-04-27 23:46:23 +0530

    go for deployment

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -26,8 +26,8 @@ export const googleAuth = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // true in prod
-      sameSite: "strict",
+      secure: true , 
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
